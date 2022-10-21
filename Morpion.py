22fbf67001b41293f46ea2160e68dbbea394af84
lucas-ribard <lucas.ribard@laplateforme.io>
2022-10-21 11:32:56 +0200

    test commit/push depuis pycharm

--- Morpion.py
+++ Morpion.py
@@ -1,2 +1,13 @@
-joueur1=input("")
-joueur1=input("")
+print("Bonjour voulez vous jouer ou voir les scores")
+menu=0
+while menu ==0:
+    choix = input("jouer   scores\n")
+    if choix == "jouer" or choix == "Jouer":
+        joueur1=input("Username 1 Croix :")
+        joueur2=input("Username 2 Rond  :")
+        menu=1
+    elif choix == "scores" or choix == "Scores" or choix == "score" or choix == "Score":
+      print("pas encore fait")
+      menu=1
+    else:
+        print (choix, "n'est pas une option valable")
